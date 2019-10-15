5d8ff945996e1e048a5bd2fb8efd42396c814068
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2019-10-16 01:03:12 +0300

    Add permutation

--- src/main/scala/ArraysAndStrings.scala
+++ src/main/scala/ArraysAndStrings.scala
@@ -3,12 +3,19 @@ import scala.annotation.tailrec
 object ArraysAndStrings {
 
   def isUnique[A](xs: List[A]): Boolean = {
-    @tailrec def go(xs: List[A]): Boolean = xs match {
+    @tailrec def go(as: List[A]): Boolean = as match {
       case Nil                       => true
       case h :: tl if tl.contains(h) => false
       case _ :: tl                   => go(tl)
     }
     go(xs)
+  }
+
+  def permutation[A](l: List[A], r: List[A]): Boolean = {
+    def occurrences(as: List[A]): Map[A, Int] =
+      as.groupMapReduce(identity)(_ => 1)(_ + _)
+
+    occurrences(l) == occurrences(r)
   }
 
 }
---
+++ src/test/scala/ArraysAndStringsSpecification.scala
@@ -0,0 +1,15 @@
+import ArraysAndStrings._
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Properties
+
+object ArraysAndStringsSpecification extends Properties("ArraysAndStrings") {
+
+  property("isUnique") = forAll { xs: List[Int] =>
+    isUnique(xs) == (xs.toSet.size == xs.size)
+  }
+
+  property("permutation") = forAll { (l: List[Int], r: List[Int]) =>
+    permutation(l, r) == (l.sorted == r.sorted)
+  }
+
+}
