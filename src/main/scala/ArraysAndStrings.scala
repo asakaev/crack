9e0d610d74ccf276747cf8655cb99f303cec608f
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2019-10-16 02:53:40 +0300

    Add palindromePermutation

--- src/main/scala/ArraysAndStrings.scala
+++ src/main/scala/ArraysAndStrings.scala
@@ -2,13 +2,13 @@ import scala.annotation.tailrec
 
 object ArraysAndStrings {
 
-  def isUnique[A](xs: List[A]): Boolean = {
+  def isUnique[A](as: List[A]): Boolean = {
     @tailrec def go(as: List[A]): Boolean = as match {
       case Nil                       => true
       case h :: tl if tl.contains(h) => false
       case _ :: tl                   => go(tl)
     }
-    go(xs)
+    go(as)
   }
 
   def permutation[A](l: List[A], r: List[A]): Boolean = {
@@ -23,5 +23,15 @@ object ArraysAndStrings {
       case (s, c) if c == ' ' => s + "%20"
       case (s, c)             => s + c
     }
+
+  def palindromePermutation[A](as: List[A]): Boolean = {
+    val s = as.foldLeft(Set.empty[A]) {
+      case (s, a) if s.contains(a) => s - a
+      case (s, a)                  => s + a
+    }
+    s.size <= 1
+  }
+
+  def oneAway[A](l: List[A], r: List[A]): Boolean = ???
 
 }
--- src/test/scala/ArraysAndStringsSpecification.scala
+++ src/test/scala/ArraysAndStringsSpecification.scala
@@ -1,8 +1,11 @@
 import ArraysAndStrings._
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
+import org.scalacheck.Test.Parameters
 
 object ArraysAndStringsSpecification extends Properties("ArraysAndStrings") {
+
+  override def overrideParameters(p: Parameters): Parameters = p.withMaxSize(5)
 
   property("isUnique") = forAll { xs: List[Int] =>
     isUnique(xs) == (xs.toSet.size == xs.size)
@@ -14,6 +17,10 @@ object ArraysAndStringsSpecification extends Properties("ArraysAndStrings") {
 
   property("urlify") = forAll { s: String =>
     urlify(s) == s.replace(" ", "%20")
+  }
+
+  property("palindromePermutation") = forAll { xs: List[Int] =>
+    palindromePermutation(xs) == xs.permutations.exists(xs => xs == xs.reverse)
   }
 
 }
